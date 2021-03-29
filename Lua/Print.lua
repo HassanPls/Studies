e38ddf0b6b4682f02b55c4a916a4f155b90cbce3
MechOfc <72605611+MechOfc@users.noreply.github.com>
2021-03-29 16:06:24 -0300

    Print.lua
    explicando sobre o print em lua

---
+++ Lua/Print.lua
@@ -0,0 +1,7 @@
+print("Olá mundo!")
+
+--[[
+imprime um valor no console/tela
+sendo esse valor uma string/número/variável/boolean
+sendo possivel também efetuar calculos dentro do print e imprimir o resultado no console/tela
+]]--
