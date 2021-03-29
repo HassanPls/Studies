dc828c10a5de709815c133a859c08bbd75f3f631
MechOfc <72605611+MechOfc@users.noreply.github.com>
2021-03-29 15:38:28 -0300

    Comentario.lua
    explicando como funciona os comentários em lua

---
+++ Lua/Comentarios.lua
@@ -0,0 +1,19 @@
+-- comentário de uma linha 
+-- caso você utilize esse método para comentar em seu script
+-- a cada linha pulada será necessário utilizar outro "--" para realizar outro comentário
+
+--[[
+
+comentário de 
+duas (ou mais)
+linhas
+
+nesse tipo de comentário é possível utilizar múltiplas linhas para explicar algo
+
+]]--
+
+-- cada comentário é ignorado na execução do código, tendo como função explicar o script
+
+
+
+
