ee043a22f30ce4a9d3fc909d1b48195462de6fe4
Pedro Luiz <plmvfilho@gmail.com>
2021-04-18 17:41:28 -0300

    Lua

--- Lua/Básico/3-Variável.lua
+++ Lua/Básico/3-Variável.lua
@@ -1,14 +1,24 @@
-local x = 1
-y = 1
+a = 10
+b = 1
+while (b < 11) do
+    local a = b + 1
+    print(a)    
+    b = b + 1
+end
 
-print(x + y)
+print(a)
 
 --[[
 
 Em lua existe 3 tipos de váriaveis:
 - Global
 - Local
-- 
+- Table
+
+váriaveis globais não precisam ser declaradas
+
+váriaveis locais precisam ser declaradas, colocando um "local" no inicio da váriavel 
+e elas só podem ser chamadas no bloco em que foram criadas
 
 
 ]]--
