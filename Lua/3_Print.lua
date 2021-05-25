5a55eb73268a301916c1e6591e82405425d07d66
Pedro Luiz <plmvfilho@gmail.com>
2021-05-25 19:53:50 -0300

    Lua

--- Lua/3_Print.lua
+++
@@ -1,7 +0,0 @@
-print("Olá mundo!")
-
---[[
-imprime um valor no console/tela
-sendo esse valor uma string/number/variável/boolean entre outros...
-sendo possivel também efetuar calculos dentro do print e imprimir o resultado 
-]]--
